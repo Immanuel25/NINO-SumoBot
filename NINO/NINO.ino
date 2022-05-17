622bc1aad1d6aef18f40387a6382299f7245a380
Immanuel25 <immanuel.raynaldo.s@gmail.com>
2022-05-17 10:07:55 +0700

    all function

--- NINO/NINO.ino
+++ NINO/NINO.ino
@@ -8,37 +8,42 @@
 int MAXSPEEDA = 800; // 800/1023 x100% duty cycle 
 int MAXSPEEDB = 800; // 800/1023 x100% duty cycle
 int INITIALSPEED = 64; 
+
 int speedA = 0;
 int speedB = 0;
 int throttle = 0;
-bool majuA = true;
-bool majuB = true;
 
 const int freq = 5000;
 const int resolution = 10; //Resolution 8, 10, 12, 15
 
-#define pinMerah 23 // GPIO23 (D23)
-#define pinBiru 22 // GPIO22 (D23)
+bool xButtonState = false;
+bool alarmState = false;
+int waktuNi = 0;
+int ganjilGenap = 0;
+
+
+#define pinMerah 33 // GPIO23 (D23)
+#define pinBiru 32 // GPIO22 (D23)
 #define pinBuzzer 18 // GPIO18 (D18)
 
 // pins for motor A
 #define RPWM_CHA 0 // CHANNEL PWM FORWARD MOTOR A
 #define RPWM_A 17 // GPIO17 (D17)
-#define R_IS_A 15 // GPIO15 (D15)
+//#define R_IS_A 15 // GPIO15 (D15)
 
 #define LPWM_CHA 1 // CHANNEL PWM REVERSE MOTOR A
 #define LPWM_A 16 // GPIO16 (D16)
-#define L_IS_A 2 // GPIO2 (D2)
+//#define L_IS_A 2 // GPIO2 (D2)
 // motor A pins end here
 
 // pins for motor B
 #define RPWM_CHB 2 // CHANNEL PWM FORWARD MOTOR B
 #define RPWM_B 26 // GPIO26 (D26)
-#define R_IS_B 12 // GPIO12 (D12)
+//#define R_IS_B 12 // GPIO12 (D12)
 
 #define LPWM_CHB 3 // CHANNEL PWM REVERSE MOTOR B
 #define LPWM_B 27 // GPIO27 (D27)
-#define L_IS_B 14 // GPIO14 (D14)
+//#define L_IS_B 14 // GPIO14 (D14)
 // motor B pins end here
 
 #define CW 1 //
@@ -64,10 +69,16 @@ void setup() {
   ledcSetup(LPWM_CHB, freq, resolution);
   ledcAttachPin(LPWM_B, LPWM_CHB);
 
-  pinMode(R_IS_A, INPUT); // ngasih tau berapa arus ngalir
-  pinMode(L_IS_A, INPUT); 
-  pinMode(R_IS_B, INPUT);  
-  pinMode(L_IS_B, INPUT);  
+//  pinMode(R_IS_A, INPUT); // ngasih tau berapa arus ngalir
+//  pinMode(L_IS_A, INPUT); 
+//  pinMode(R_IS_B, INPUT);  
+//  pinMode(L_IS_B, INPUT);  
+
+  pinMode(pinMerah, OUTPUT);
+  pinMode(pinBiru, OUTPUT);
+  pinMode(pinBuzzer, OUTPUT);
+
+  ledcAttachPin(pinBuzzer, 4);
 }
 
 void loop() {
@@ -87,47 +98,77 @@ void loop() {
       speedB = map(speedB, 0, 255, 0, MAXSPEEDB);
   
       }
+    
+    if (speedA>20){ // motor A CW
+      ledcWrite(LPWM_CHA, 0);
+      ledcWrite(RPWM_CHA, speedA);
     }
+    else if (speedA<-20){ // motor A CCW
+      ledcWrite(RPWM_CHA, 0);
+      ledcWrite(LPWM_CHA, -speedA);
+    }
+    else{ // motor A berenti
+      ledcWrite(RPWM_CHA, 0);
+      ledcWrite(LPWM_CHA, 0);
+    }
+    
+    if (speedB>20){ // motor B CCW
+      ledcWrite(RPWM_CHB, 0);
+      ledcWrite(LPWM_CHB, speedB);
+    }
+    else if (speedB<-20){ // motor B CW
+      ledcWrite(LPWM_CHB, 0);
+      ledcWrite(RPWM_CHB, -speedB);
+    }
+    else{ // motor B berenti
+      ledcWrite(RPWM_CHB, 0);
+      ledcWrite(LPWM_CHB, 0);
+    }
+    
+    if (debug==1){
+      Serial.print("speedA: ");
+      Serial.print(speedA);
+      Serial.print(" speedB: ");
+      Serial.println(speedB);
+      Serial.println("");
+      delay(100);
+    }
+ 
+    if (PS4.Cross() and xButtonState == false) {
+      if (alarmState == true){ //matiin alarm
+        digitalWrite(pinMerah, LOW);
+        digitalWrite(pinBiru, LOW);
+        ledcWrite(4, 0);
+        alarmState = false;
+      }
+      else {
+        alarmState = true;
+        waktuNi = millis();
+      }
+    }
+    
+    xButtonState = PS4.Cross();
+  
+    if (alarmState == true){
+      ganjilGenap = (millis() - waktuNi)/500;
+      ganjilGenap = ganjilGenap%2;
+      
+      if (ganjilGenap == 0){
+        digitalWrite(pinMerah, LOW);
+        digitalWrite(pinBiru, HIGH);
+        ledcWriteTone(4, 50);
+      }
+      else{
+        digitalWrite(pinMerah, HIGH);
+        digitalWrite(pinBiru, LOW);
+        ledcWriteTone(4, 500);
+      }
+    }
+  }
   else{
-  Serial.println("disconnected");
-  delay(100);
-  }
-  
-  if (speedA>20){ // motor A CW
-    ledcWrite(LPWM_CHA, 0);
-    ledcWrite(RPWM_CHA, speedA);
-  }
-  else if (speedA<-20){ // motor A CCW
-    ledcWrite(RPWM_CHA, 0);
-    ledcWrite(LPWM_CHA, -speedA);
-  }
-  else{ // motor A berenti
-    ledcWrite(RPWM_CHA, 0);
-    ledcWrite(LPWM_CHA, 0);
-  }
-  
-  if (speedB>20){ // motor B CCW
-    ledcWrite(RPWM_CHB, 0);
-    ledcWrite(LPWM_CHB, speedB);
-  }
-  else if (speedB<-20){ // motor B CW
-    ledcWrite(LPWM_CHB, 0);
-    ledcWrite(RPWM_CHB, -speedB);
-  }
-  else{ // motor B berenti
-    ledcWrite(RPWM_CHB, 0);
-    ledcWrite(LPWM_CHB, 0);
-  }
-  
-  if (debug==1){
-    Serial.print("speedA: ");
-    Serial.print(speedA);
-    Serial.print(" speedB: ");
-    Serial.println(speedB);
-    Serial.println("");
+    Serial.println("disconnected");
     delay(100);
   }
-  
 }
 
 
